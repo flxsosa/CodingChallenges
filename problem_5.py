fa448f16162c8b39ba55eaf281fd2fd8b05519e6
twizzler <felixanthonysosa@gmail.com>
2020-06-01 12:46:51 -0400

    potential solution. Not sure if I'm allowed to assume all lists are expected to start with 1.

---
+++ problem_5.py
@@ -0,0 +1,23 @@
+'''
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space.
+In other words, find the lowest positive integer that does not exist in the array.
+The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+'''
+
+def missing_int(arr):
+    curr_int = 1
+    for item in arr:
+        if item == curr_int + 1:
+            curr_int = item
+    return curr_int+1
+
+print(missing_int([3,4,-1,1]))
+print(missing_int([1,2,0]))
